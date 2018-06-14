03e7559d79da8d51e7ff1b879333ca0445cbae26
Carenalga <mapedorr@gmail.com>
2018-06-14 00:51:24 -0500

    Primeros pasos creando cartas
    Se crea ScriptableObject para tener información de carta e ilustración.
    Se crea Prefab de UI para carta. Todas se crearán a partir de dicho Prefab.

---
+++ Huevo Saurio/Assets/Scripts/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+	// ══════════════════════════════════════════════════════════════ PUBLICS ════
+	public new string name;
+	public int number;
+	public Sprite illustration;
+
+	// ══════════════════════════════════════════════════════════════ METHODS ════
+	// TODO: define methods
+}
---
+++ Huevo Saurio/Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+	// ══════════════════════════════════════════════════════════════ PUBLICS ════
+	public Card card;
+	public Image illustrationImage;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start ()
+	{
+		illustrationImage.sprite = card.illustration;
+	}
+}
